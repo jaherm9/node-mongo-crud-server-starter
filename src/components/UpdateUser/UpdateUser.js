47e239793fda98831d7392234d05430872d82269
shaka <jaherm9@gmail.com>
2021-10-24 00:16:58 +0600

    CRUD Operation completed

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {useParams} from 'react-router';
 
 const UpdateUser = () => {
+    const [user, setUser] = useState({})
+    const {id} = useParams()
+
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data => setUser(data))
+    }, [])
+
+    // update user
+    const handleNameChange = e =>{
+        // console.log(e.target.value)
+        const updatedName = e.target.value;
+        const updatedUser = {name: updatedName, email: user.email};
+        setUser(updatedUser);
+    }
+
+    const handleEmailChange = e =>{  
+        const updateEmail = e.target.value;
+        // const updatedUser = {...user};
+        // updatedUser.email = updateEmail;
+        const updatedUser = {name: user.name, email: updateEmail}
+        setUser(updatedUser)
+    }
+
+    const handleUpdateUser = e =>{
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            header: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount > 0){
+                alert('Updated Successfully.')
+                setUser({});
+            }
+        })
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update: {user.name}</h2>
+            <p><small>{id}</small></p>
+            <form onSubmit={handleUpdateUser}>
+                <input type="text" onChange={handleNameChange} value={user.name} />
+                <input type="email" onChange={handleEmailChange} value={user.email} />
+                <input type="submit" value="Update" />
+            </form>
         </div>
     );
 };
